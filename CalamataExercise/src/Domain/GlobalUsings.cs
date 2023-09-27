2b032c5630b56dc3418fadc6b5fb54bb7d30748a
developer x <developerx@hotmail.com>
2023-09-28 00:04:53 +0200

    Project clean up

--- CalamataExercise/src/Domain/GlobalUsings.cs
+++ CalamataExercise/src/Domain/GlobalUsings.cs
@@ -1,4 +1,3 @@
 ﻿global using CalamataExercise.Domain.Common;
 global using CalamataExercise.Domain.Enums;
 global using CalamataExercise.Domain.Exceptions;
-global using CalamataExercise.Domain.ValueObjects;
